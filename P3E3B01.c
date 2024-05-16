69c8736c3bc292546f26f977342bd8d50366994c
920530ray <u11002201@o365.ttu.edu.tw>
2024-05-16 20:45:47 +0800

    b. 電腦排位

--- P3E3B01.c
+++ P3E3B01.c
@@ -77,7 +77,117 @@ int length=8,height=0;			//設定亂數座位上下限
 	printf("(按下任意鍵回到主選單)\n");									//提示回到主選單 
 	getch(fault);
 	goto menu;
-	
+	case'B':			//b.電腦排位 																
+	case'b':
+		printf("請輸入人數(1~4)=>");
+		scanf("%d",&r);
+	while(r<1||r>4)			//判斷和要求的人數有沒有不同 												
+	{
+		system("CLS");
+		printf("人數錯誤，請重新輸入！\n");				//判斷為否，重新輸入							 
+		printf("請輸入人數(1~4)=>");			//印出輸入人數 
+		scanf("%d",&r);
+	}
+		system("CLS");
+		z=0;
+		while (z<=1)			//設定位置次數上限 													
+		{
+			while(e!=1)			//產生座位的亂數													 
+			{
+				q= rand() % (length-height+1)+height;
+				w= rand() % (length-height+1)+height;
+					if (seat[q][w] !=1)			//將產生的亂數座位安排成選中的座位 											
+					{
+						if (r==1)
+						{
+							seat[q][w]=2;			//1個座位 										
+							e=1;
+						}
+						else if (r==2 && (w+1)<=8 && seat[q][w+1] != 1)
+						{
+							seat[q][w]=2;			//2個座位 										
+							seat[q][w+1]=2;
+							e= 1;
+						}
+						else if (r==3 && (w+ 2)<=8 && seat[q][w+1] !=1 && seat[q][w+2] != 1)
+						{
+							seat[q][w] = 2;			//3個座位									
+							seat[q][w+1] = 2;
+							seat[q][w+2] = 2;
+							e=1;
+						}
+						else if (r==4 && (w+ 1)<= 8 && (q+1)>=0 && seat[q][w+1] != 1 && seat[q+1][w] != 1 && seat[q+1][w+1] != 1)
+						{
+							seat[q][w] = 2;			//4個座位								 
+							seat[q+1][w] = 2;
+							seat[q][w+1] = 2;
+							seat[q+1][w+1] = 2;
+							e=1;
+						}
+					}
+				else
+					e=0;			//電腦沒找到符合條件的位置 													
+			}
+				printf("\\123456789\n");			//印出座位表
+				for (q=8;q>=0;q--)
+				{
+					printf("%d",q+1);
+					for (w=0;w<=8;w++)
+					{
+					if (seat[q][w] == 0)
+						printf("-");
+					else if (seat[q][w] == 1)
+						printf("*");
+					else
+						printf("@");
+					}
+					printf("\n");
+				}
+				printf("接受此電腦排位嗎(y / n)？\n");			//印出是否接受 
+				printf("(不接受次數達兩次將退回主選單)\n");
+				fflush(stdin);
+				scanf("%c", &ch);
+			while(ch != 'Y' && ch != 'y' && ch != 'N' && ch != 'n')				//判斷字元是否是需要的 
+				{
+					system("CLS");
+					printf("錯誤訊息，請重新輸入！\n");			//輸入錯誤，需要使用者重新輸入 
+					printf("接受此電腦排位嗎(y / n)？");
+					scanf("%c", &ch);
+				}
+				
+				if(ch == 'Y' || ch == 'y')			//判斷輸入者的選擇 										
+				{
+					for (q=8;q>=0;q--)			//將挑中的位置變成已預訂的座位 										
+					{
+						for (w=0;w<= 8;w++)
+						{
+							if (seat[q][w]==2)
+								seat[q][w]=1;
+						}
+					}
+					e=0;
+					goto menu;
+				}
+				else
+				{
+					z++;
+					e=0;
+					for (q=8;q>=0;q--)			//將沒挑到的位置放棄										
+					{
+						for (w=0;w<=8;w++)
+						{
+							if (seat[q][w] == 2)
+								seat[q][w] = 0;
+						}
+					}
+					system("CLS");
+				}	
+				}
+			printf("\n已選擇兩次不接受，退回主選單。");
+			printf("\n(按下任意鍵回到主選單)\n");			//回到主選單 
+			getch(fault);
+			goto menu;
+			
 	}
 	return 0;
 }
