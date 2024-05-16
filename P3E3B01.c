ef46fa0bee98100a2f26c9a2bd059e2e6bcc5654
920530ray <u11002201@o365.ttu.edu.tw>
2024-05-16 14:27:09 +0800

    新增檔案及個人封面

---
+++ P3E3B01.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
+int main(void)
+{
+		printf("                                                                           \n"); 
+	printf("       EEEEEEEEEE     3333333333      BBBBBBBBBB       0000000000      111 \n"); 
+	printf("       EEEEEEEEEE     33333333333     BBBBBBBBBBB      0000000000      111 \n"); 
+	printf("       EEE                     333    BBB      BBB     000    000      111 \n"); 
+	printf("       EEE                     333    BBB      BBB     000    000      111 \n"); 
+	printf("       EEEEEEEEEE     3333333333      BBBBBBBBBB       000    000      111 \n"); 
+	printf("       EEEEEEEEEE     3333333333      BBBBBBBBBB       000    000      111 \n"); 
+	printf("       EEE                     333    BBB      BBB     000    000      111 \n"); 
+	printf("       EEE                     333    BBB      BBB     000    000      111 \n"); 
+	printf("       EEEEEEEEEE     33333333333     BBBBBBBBBBB      0000000000      111 \n"); 
+	printf("       EEEEEEEEEE     3333333333      BBBBBBBBBB       0000000000      111 \n");   
+	printf("                                                                           \n");  //顯示個人風格封面 
+
+	system("pause");  //螢幕畫面暫停，並等待使用者按任意鍵
+	system("CLS");  //清除畫面 
+}
