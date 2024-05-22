5b77cfa3299c601e548a265c467745313868c747
920530ray <u11002201@o365.ttu.edu.tw>
2024-05-22 21:43:26 +0800

    新增檔案及顯示個人風格封面

---
+++ P4E3B01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+int main(void)
+{	
+	printf("                                                                           \n"); 
+	printf("       EEEEEEEEEE     3333333333      BBBBBBBBBB       0000000000      111 \n"); 
+	printf("       EEEEEEEEEE     33333333333     BBBBBBBBBBB      0000000000      111 \n"); 
+	printf("       EEE                     333    BBB      BBB     000    000      111 \n"); 
+	printf("       EEE                     333    BBB      BBB     000    000      111 \n"); 
+	printf("       EEEEEEEEEE     3333333333      BBBBBBBBBB       000    000      111 \n"); 
+	printf("       EEEEEEEEEE     3333333333      BBBBBBBBBB       000    000      111 \n"); 
+	printf("       EEE                     333    BBB      BBB     000    000      111 \n"); 
+	printf("       EEE                     333    BBB      BBB     000    000      111 \n"); 
+	printf("       EEEEEEEEEE     33333333333     BBBBBBBBBBB      0000000000      111 \n"); 
+	printf("       EEEEEEEEEE     3333333333      BBBBBBBBBB       0000000000      111 \n");   
+	printf("                                                                           \n");  //顯示個人風格封面 
+}
+ 
